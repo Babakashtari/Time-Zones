a7f93331d5f7d44627ee0074845e2c303a59597a
babakashtari <ashtaribabak@rocketmail.com>
2019-11-06 22:51:39 +0330

    added a zero for numbers below 9

--- clock.js
+++ clock.js
@@ -21,9 +21,9 @@ const timeCount = () => {
   const minutes = Iranian_Time.getMinutes();
   const seconds = Iranian_Time.getSeconds();
 
-  hours_number[0].innerHTML = hours;
-  minutes_number[0].innerHTML = minutes;
-  seconds_number[0].innerHTML = seconds;
+  hours_number[0].innerHTML = (hours > 9 && hours) || "0" + hours;
+  minutes_number[0].innerHTML = (minutes > 9 && minutes) || "0" + minutes;
+  seconds_number[0].innerHTML = (seconds > 9 && seconds) || "0" + seconds;
 
   hours_handle[0].style.transform = `rotate(${hours * 30 + 180}deg)`;
   minutes_handle[0].style.transform = `rotate(${minutes * 6 + 180}deg)`;
@@ -36,9 +36,12 @@ const timeCount = () => {
   const minutes_France = French_Time.getMinutes();
   const seconds_France = French_Time.getSeconds();
 
-  hours_number[1].innerHTML = hours_France;
-  minutes_number[1].innerHTML = minutes_France;
-  seconds_number[1].innerHTML = seconds_France;
+  hours_number[1].innerHTML =
+    (hours_France > 9 && hours_France) || "0" + hours_France;
+  minutes_number[1].innerHTML =
+    (minutes_France > 9 && minutes_France) || "0" + minutes_France;
+  seconds_number[1].innerHTML =
+    (seconds_France > 9 && seconds_France) || "0" + seconds_France;
 
   hours_handle[1].style.transform = `rotate(${hours_France * 30 + 180}deg)`;
   minutes_handle[1].style.transform = `rotate(${minutes_France * 6 + 180}deg)`;
@@ -52,9 +55,11 @@ const timeCount = () => {
   const minutes_LA = LA_Time.getMinutes();
   const seconds_LA = LA_Time.getSeconds();
 
-  hours_number[2].innerHTML = hours_LA;
-  minutes_number[2].innerHTML = minutes_LA;
-  seconds_number[2].innerHTML = seconds_LA;
+  hours_number[2].innerHTML = (hours_LA > 9 && hours_LA) || "0" + hours_LA;
+  minutes_number[2].innerHTML =
+    (minutes_LA > 9 && minutes_LA) || "0" + minutes_LA;
+  seconds_number[2].innerHTML =
+    (seconds_LA > 9 && seconds_LA) || "0" + seconds_LA;
 
   hours_handle[2].style.transform = `rotate(${hours_LA * 30 + 180}deg)`;
   minutes_handle[2].style.transform = `rotate(${minutes_LA * 6 + 180}deg)`;
